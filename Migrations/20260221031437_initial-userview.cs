135c2577e82e98adec3bd7d23e90fb958775cd66
Paras Panchal <paraspanchal5555@gmail.com>
2026-02-21 07:30:30 +0400

    Commit for Migrations and product images

---
+++ Migrations/20260221031437_initial-userview.cs
@@ -0,0 +1,364 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceManagementPro.Migrations
+{
+    public partial class initialuserview : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerName",
+                table: "Customer",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerDetail",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "AccountNumber",
+                table: "Customer",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BillingAddress",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactPerson",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerType",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "GSTIN",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LogoImagePath",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShippingAddress",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ZipCode",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EwayBillNo",
+                table: "Bill",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "GrandTotalAmount",
+                table: "Bill",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InternalNotes",
+                table: "Bill",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InvoiceNumber",
+                table: "Bill",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "POBillNo",
+                table: "Bill",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ShippingCost",
+                table: "Bill",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Bill",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "SubTotal",
+                table: "Bill",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TermsAndConditions",
+                table: "Bill",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalDiscount",
+                table: "Bill",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalTax",
+                table: "Bill",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VehicleNo",
+                table: "Bill",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "BillItem",
+                columns: table => new
+                {
+                    BillItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BillId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    ItemRate = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DiscountPercentage = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TaxPercentage = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    LineTotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    HSNCode = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BillItem", x => x.BillItemId);
+                    table.ForeignKey(
+                        name: "FK_BillItem_Bill_BillId",
+                        column: x => x.BillId,
+                        principalTable: "Bill",
+                        principalColumn: "BillId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BillItem_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bill_CustomerId",
+                table: "Bill",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BillItem_BillId",
+                table: "BillItem",
+                column: "BillId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BillItem_ProductId",
+                table: "BillItem",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bill_Customer_CustomerId",
+                table: "Bill",
+                column: "CustomerId",
+                principalTable: "Customer",
+                principalColumn: "CustomerId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bill_Customer_CustomerId",
+                table: "Bill");
+
+            migrationBuilder.DropTable(
+                name: "BillItem");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bill_CustomerId",
+                table: "Bill");
+
+            migrationBuilder.DropColumn(
+                name: "BillingAddress",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "ContactPerson",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerType",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "GSTIN",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "LogoImagePath",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingAddress",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "ZipCode",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "EwayBillNo",
+                table: "Bill");
+
+            migrationBuilder.DropColumn(
+                name: "GrandTotalAmount",
+                table: "Bill");
+
+            migrationBuilder.DropColumn(
+                name: "InternalNotes",
+                table: "Bill");
+
+            migrationBuilder.DropColumn(
+                name: "InvoiceNumber",
+                table: "Bill");
+
+            migrationBuilder.DropColumn(
+                name: "POBillNo",
+                table: "Bill");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingCost",
+                table: "Bill");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Bill");
+
+            migrationBuilder.DropColumn(
+                name: "SubTotal",
+                table: "Bill");
+
+            migrationBuilder.DropColumn(
+                name: "TermsAndConditions",
+                table: "Bill");
+
+            migrationBuilder.DropColumn(
+                name: "TotalDiscount",
+                table: "Bill");
+
+            migrationBuilder.DropColumn(
+                name: "TotalTax",
+                table: "Bill");
+
+            migrationBuilder.DropColumn(
+                name: "VehicleNo",
+                table: "Bill");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerName",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerDetail",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "AccountNumber",
+                table: "Customer",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+        }
+    }
+}
